4527de2386baa2af581ec9c3b2d70c7cecc627bc
garethlvjones <garethlvjones@gmail.com>
2018-07-17 12:03:37 +1000

    import.py done

--- 01_project/import.py
+++ 01_project/import.py
@@ -19,17 +19,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-# # test fill database with books.csv stuff
-# def loadBooks():
-#     f = open(books.csv)
-#     reader = csv.reader(f)
-
-#     for isbn,title,author,year in reader:
-#         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-#             {"isbn": isbn, "title": title, "author": author, "year": year})
-#         print(f"Added {title} by {author} with {isbn} isbn to db")
-#     db.commit()
-
-# loadBooks()
