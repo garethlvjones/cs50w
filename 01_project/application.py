a8833e9d9ea5c1d32174daf059ed8b57f031c7e5
garethlvjones <garethlvjones@gmail.com>
2018-07-20 18:20:30 +1000

    started login buttons & css

--- 01_project/application.py
+++ 01_project/application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -16,6 +16,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 Session(app)
 
 # Set up database
@@ -23,9 +24,11 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 # Test get from goodreads
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "t00295auIVdosK82l8e8Q", "isbns": "9781632168146"})
-print(res.json())
+# res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "t00295auIVdosK82l8e8Q", "isbns": "9781632168146"})
+# print(res.json())
 
+
+# Routes
 @app.route("/")
 def index():
-    return "Project 1: TODO2"
+    return render_template("index.html")
