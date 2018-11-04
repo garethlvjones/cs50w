756fd2096a9f8c4d0d059f61a0f29463e39a517c
garethlvjones <garethlvjones@gmail.com>
2018-11-04 16:46:17 +1100

    started project 2

--- 02_project/application.py
+++ 02_project/application.py
@@ -1,13 +1,19 @@
-import os
+import os, random, string
 
-from flask import Flask
+from flask import Flask, flash, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+
+# Session(app)
 socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
